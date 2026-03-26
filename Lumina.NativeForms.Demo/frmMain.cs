b56d49265167712405316bf63b7b52db34789e59
maikebing <mysticboy@live.com>
2026-03-27 02:23:06 +0800

    我要了

---
+++ Lumina.NativeForms.Demo/frmMain.cs
@@ -0,0 +1,12 @@
+ 
+
+namespace Lumina.NativeForms.Demo
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
